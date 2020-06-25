06265740aaab96048280d7152cb5e8e7344a6ade
GeorgiosP <papadoc7@users.noreply.github.com>
2020-06-25 14:38:01 +0100

    Update Article.test.js

--- test/Article.test.js
+++ test/Article.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Article from '../src/components/Article';
 
-it('Question component is rendering', () => {
+it('Article component is rendering', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Article />, div);
   ReactDOM.unmountComponentAtNode(div);
