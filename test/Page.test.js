0b620608c4e6f5cb1261605bfd4706bf5e04c13b
GeorgiosP <papadoc7@users.noreply.github.com>
2020-06-25 14:37:44 +0100

    Update Page.test.js

--- test/Page.test.js
+++ test/Page.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from '../src/components/Page';
 
-it('Question component is rendering', () => {
+it('Page component is rendering', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Page />, div);
   ReactDOM.unmountComponentAtNode(div);
